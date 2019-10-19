5ddce93fbd96e2c6887ee26736f75c5f0e0baa55
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-20 04:39:22 +0700

    Add project files.

---
+++ OrderServiceTests5_11/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NUnit.Framework;
+using Testing_the_Interaction_5_11.Mocking;
+
+namespace OrderServiceTests5_11
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+       [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+            var storage = new Mock<IStorage>();
+            var service = new OrderService(storage.Object);
+
+            var order = new Order();
+            service.PlaceOrder(order);
+
+            storage.Verify(s => s.Store(order));
+
+
+        }
+    }
+}
---
+++ Testing_the_Interaction_5_11/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_the_Interaction_5_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
